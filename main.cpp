a7450c62b28178ba6e75c3d36fe98daf09022a8a
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-12 00:11:06 +0000

    Initialize arrays

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // declare and initialize arrays
+  int resistance[5] = {16, 27, 39, 56, 81};
+
+  const int CURR = 5;
+  double current[CURR];
+
+  const int POW = 5;
+  double power[POW];
+
+  // for loop for user input
+
+ 
+}
